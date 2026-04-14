71173c107bb735bd0559346d45ca97823ac001c7
jacobinav <jacob.sjodin@nav.no>
2026-04-14 15:43:15 +0200

    Enhetsquery

--- uttrekk/alle_tiltak.sql
+++ uttrekk/alle_tiltak.sql
@@ -61,3 +61,14 @@ pivot (
 )
 order by aarmnd_dato
 ;
+
+select ned.aarmnd_dato , ned.nav_enhet_navn, count(*) as cnt
+from dvh_arb_tiltak.FAK_TILTAK_UPRIORITERT_MND tiltak
+inner join nedbrytning ned
+    on ned.fk_person1 = tiltak.fk_person1
+    and ned.aarmnd = substr(tiltak.periode, 1, 6)
+left join dvh_arb_tiltak.dim_tiltakstype dim
+on tiltak.fk_dim_tiltakstype = dim.pk_dim_tiltakstype
+where tiltaksnavn = 'Midlertidig lønnstilskudd'
+group by ned.aarmnd_dato, ned.nav_enhet_navn
+;
